f3779a38c4ae8bfc6f868c23dca0683b6af8792a
Edevar <edevar.ads@gmail.com>
2024-09-24 18:04:38 -0300

    Iniciado JS

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const optionImages = document.querySelectorAll(".option-image")
+const container = document.querySelector(".container")
+const resultText = document.querySelector(".result-text")
 
-console.log (optionImages)
+function handleOptionClick(event){
+    const clickdImage = event.currentTarget
+
+    container.classList.add("start")
+    resultText.textContent = "..."
+
+    setTimeout (() => {
+        container.classList.remove("start")
+
+
+        resultText.textContent = "X ganhou"
+    }, 2000); // 2000milisegundos = 2 segundos
+}
+
+
+
+
+
+
+
+optionImages.forEach( img => {
+    img.addEventListener("click", handleOptionClick)
+
+})
+
+
+// Parei no minuto 52 do video
